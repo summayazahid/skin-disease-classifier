f559546ab56b03a4d74c64cf30aa67b91227332a
Summaya Zahid <158520104+summayazahid@users.noreply.github.com>
2025-04-20 23:11:33 +0500

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1swlXlfJcfAz2ueGqNfmMZF_7hOuNTIYG
 """
 
-!pip install streamlit
+
 
 import streamlit as st
 import numpy as np
